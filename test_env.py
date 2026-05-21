aa07867e8d174e13d0643b8dba1b46c177d18387
PandiKabilesh2006 <pandikabilesh2270157@gmail.com>
2026-05-21 18:15:38 +0530

    Cleaned Repository

--- test_env.py
+++
@@ -1,3 +0,0 @@
-from app.config.settings import OPENAI_API_KEY
-
-print(OPENAI_API_KEY)
--- test_search.py
+++
@@ -1,6 +0,0 @@
-from app.search.tavily_search import search_investors
-
-results = search_investors("AI seed investors")
-
-# print(results['results'][0])
-print(results)
